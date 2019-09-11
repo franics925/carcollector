0b8f7f99ff4cb50e64376efd6562039f9fc37e3d
Gregory Redig <gregoryredig@Gregorys-MBP.attlocal.net>
2019-09-11 00:52:46 -0700

    add maintanence model and make migrations

--- main_app/models.py
+++ main_app/models.py
@@ -3,6 +3,12 @@ from django.urls import reverse
 
 # Create your models here.
 
+MNTYPES = (
+    ('C', 'Checkup'),
+    ('O', 'Oil Change'),
+    ('R', 'Repair')
+)
+
 class Car(models.Model):
     make = models.CharField(max_length=50)
     model = models.CharField(max_length=50)
@@ -13,3 +19,18 @@ class Car(models.Model):
 
     def get_absolute_url(self):
         return reverse('detail', kwargs={'car_id': self.id})
+
+
+class Maintenance(models.Model):
+    date = models.DateField()
+    mnType = models.CharField(
+        max_length=1,
+        choices=MNTYPES,
+        default=MNTYPES[0][0]
+    )
+    details = models.CharField(max_length=200)
+    cost = models.IntegerField()
+    car = models.ForeignKey(Car, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.get_maintenance_desplay()} on {self.date}"
