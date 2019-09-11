5cb38cac2ea51f4b50fef71ea932723afbb0a04f
Gregory Redig <gregoryredig@Gregorys-MBP.attlocal.net>
2019-09-11 01:04:17 -0700

    add mileage attribute to Car class, create maintenance model

--- main_app/models.py
+++ main_app/models.py
@@ -13,6 +13,7 @@ class Car(models.Model):
     make = models.CharField(max_length=50)
     model = models.CharField(max_length=50)
     year = models.IntegerField()
+    mileage = models.IntegerField()
 
     def __str__(self):
         return self.model
